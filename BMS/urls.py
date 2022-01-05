76854ede0caf9ea3b3317d94616234d36a00bb69
camshekhar <s.shekhar6057@gmail.com>
2022-01-05 17:00:54 +0530

    setting

--- BMS/urls.py
+++ BMS/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('', include('home.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
