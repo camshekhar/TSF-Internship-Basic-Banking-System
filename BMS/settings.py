e63431d93230a735543148bcbc3b0d547c96061d
camshekhar <s.shekhar6057@gmail.com>
2022-01-05 16:30:53 +0530

    setting

--- BMS/settings.py
+++ BMS/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-django_heroku.settings(locals())
+
 
 #Managing Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
