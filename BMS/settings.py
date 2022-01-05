acc123089d9668c78388f9a7ceb85d07060f81e5
camshekhar <s.shekhar6057@gmail.com>
2022-01-05 17:14:05 +0530

    setting

--- BMS/settings.py
+++ BMS/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # ]   
  
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 
